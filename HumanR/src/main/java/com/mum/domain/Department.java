7e993ccde45d9448fdf944a2f94092b9949e002f
Nabin Puri <abiralyatri44@gmail.com>
2016-08-28 17:01:07 -0500

    configuration added by nabin

--- HumanR/src/main/java/com/mum/domain/Department.java
+++ HumanR/src/main/java/com/mum/domain/Department.java
@@ -3,11 +3,19 @@
  */
 package com.mum.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * @author ashok
  *
  */
+@Entity
 public class Department {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private long departmentId;
 	private String departmentName;
 	public long getDepartmentId() {
